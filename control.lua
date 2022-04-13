ead1f4594672da1cbd7ad16eed5542c0eb9ecc78
json <json.commits@gmail.com>
2022-04-13 22:20:07 +0800

    Added a check for grid availability.

--- control.lua
+++ control.lua
@@ -8,30 +8,36 @@ function disable_belt_immunity(grid)
 end
 
 script.on_event(defines.events.on_tick,
-    function()
-        for _, val in pairs(game.connected_players) do
-            local player = val
+        function()
+            for _, val in pairs(game.connected_players) do
+                local player = val
 
-            if player.get_inventory(defines.inventory.character_armor) == nil then
-                return
-            end
+                if player.get_inventory(defines.inventory.character_armor) == nil then
+                    return
+                end
 
-            local player_armor = player.get_inventory(defines.inventory.character_armor)[1]
-            if not player_armor.valid_for_read then
-                return
-            end
 
-            local character = player.character
-            local area_around_player = player.character.bounding_box
-            for _, v in pairs(player.surface.find_entities(area_around_player)) do
-                if string.find(v.name, 'belt') then
-                    if character.direction == v.direction and character.walking_state['walking']then
-                        disable_belt_immunity(player_armor.grid)
-                        break
+
+                local player_armor = player.get_inventory(defines.inventory.character_armor)[1]
+                if not player_armor.valid_for_read then
+                    return
+                end
+
+                if player_armor.grid == nil then
+                    return
+                end
+
+                local character = player.character
+                local area_around_player = player.character.bounding_box
+                for _, v in pairs(player.surface.find_entities(area_around_player)) do
+                    if string.find(v.name, 'belt') then
+                        if character.direction == v.direction and character.walking_state['walking']then
+                            disable_belt_immunity(player_armor.grid)
+                            break
+                        end
                     end
                 end
-            end
 
+            end
         end
-    end
 )
